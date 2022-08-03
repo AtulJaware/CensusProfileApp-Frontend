db3e7ce3e35c7fb9ca09eb5451580d6b685d428e
Atul Jaware Patil <atuljaware.com>
2022-08-03 22:59:25 +0530

    Validations modified

--- src/Screens/Auth/userRegister.jsx
+++ src/Screens/Auth/userRegister.jsx
@@ -3,14 +3,12 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { ServiceCall } from "../../Services/RegisterServiceMethods";
 import { UserApiConstant } from "../../Constants/ApiConstant";
-import { registerAction } from "../../AppState/Actions/loginactions";
-import { dispatch } from "react";
 import Joi from "joi-browser";
 
 const UserRegister = () => {
   const navigate = useNavigate();
   const [users, setUser] = useState({
-    userId: "",
+    userId: "1",
     firstName: "",
     lastName: "",
     dob: "",
@@ -26,8 +24,10 @@ const UserRegister = () => {
   const schema = {
     firstName: Joi.string().alphanum().min(5).max(30).required(),
     lastName: Joi.string().alphanum().min(5).max(30).required(),
-    contactNo: Joi.number().integer().max(10).required(),
+    contactNo: Joi.number().integer().max(9999999999).required(),
     dob: Joi.date().iso().required(),
+    userId: Joi.string().required(),
+    role: Joi.string().required(),
     email: Joi.string()
       .email({
         minDomainSegments: 2,
@@ -69,19 +69,9 @@ const UserRegister = () => {
     setErrors(validate());
 
     if (errors) return;
-
-    // dispatch login action to rest api
-    dispatch(registerAction(users));
-
-    setTimeout(() => {
-      if (userl.users.loggedIn) {
-        ServiceCall.postApi(UserApiConstant.registerUser, users);
-        navigate("/login");
-      } else {
-        console.log("*********" + userl.errMsg);
-        setErrRes(userl.errMsg);
-      }
-    }, 1000);
+    ServiceCall.postApi(UserApiConstant.registerUser, users);
+    navigate("/login");
+    setErrRes(userl.errMsg);
   };
   console.log(users);
   return (
