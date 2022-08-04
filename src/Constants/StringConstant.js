f160a9a59ad747b1a029d9d5b9fce764bc080f06
Manisha-1231 <manisha.gadigantla@capgemini.com>
2022-08-04 10:06:52 +0530

    string constant

--- src/Constants/StringConstant.js
+++ src/Constants/StringConstant.js
@@ -1,13 +1,13 @@
 export const StringConstant = {
     updateAppMessage : " Updateded Application Successfully!",
     successMessage: "logged in successfully!",
-  successMessage: "logged in successfully!",
-  deleteAlert: "Are you sure you want to delete",
-  memberAdded: "Member Added successfully with ID ",
-  memberUpdated: "Member Updated successfully with ID ",
-  memberDeleted: "Member deleted successfully with Id ",
-  userAdded: "User Added successfully with ID ",
-  userUpdated: "User Updated successfully with ID "
+    successMessage: "logged in successfully!",
+    deleteAlert: "Are you sure you want to delete",
+    memberAdded: "Member Added successfully with ID ",
+    memberUpdated: "Member Updated successfully with ID ",
+    memberDeleted: "Member deleted successfully with Id ",
+    userAdded: "User Added successfully with ID ",
+    userUpdated: "User Updated successfully with ID "
 };
  
 export const USER_TYPE= {
